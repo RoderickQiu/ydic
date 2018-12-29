ab372441eb237ecee1ef5c44deeb9485b5296e56
RoderickQiu <scrisqiu@hotmail.com>
2018-12-29 21:30:34 +0800

    1.1.0 update

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ let win
 
 function createWindow() {
   // 创建浏览器窗口。
-  win = new BrowserWindow({ width: 768, height: 300, frame: false, show: false, center: true, titleBarStyle: 'customButtonsOnHover' })//titleBarStyle: customButtonsOnHover, 返回一个隐藏标题栏的全尺寸内容窗口
+  win = new BrowserWindow({ width: 768, height: 300, frame: false, show: false, center: true, hasShadow: true, thickFrame: true, titleBarStyle: 'customButtonsOnHover', webPreferences: { nodeIntegration: true } })//titleBarStyle: customButtonsOnHover, 返回一个隐藏标题栏的全尺寸内容窗口
 
   // 然后加载应用的 index.html。
   win.loadFile('index.html')
 
   //在加载页面时，渲染进程第一次完成绘制时，会发出 ready-to-show 事件。在此事件后显示窗口将没有视觉闪烁
   win.once('ready-to-show', () => {
-    win.show()
+    win.show();
+    //win.webContents.openDevTools()
   })
 
   // 当 window 被关闭，这个事件会被触发。
@@ -24,6 +25,15 @@ function createWindow() {
     win = null
   })
 }
+
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
@@ -48,7 +58,7 @@ ipcMain.on('window-min', function () {
 //窗口有道词典尺寸，通过ipc传递
 ipcMain.on('window-dict', function () {
   if (win != null) {
-    win.setContentSize(768, 300); 
+    win.setContentSize(768, 300);
     win.center();
   }
 })
@@ -56,7 +66,7 @@ ipcMain.on('window-dict', function () {
 //窗口韦氏词典尺寸，通过ipc传递
 ipcMain.on('window-englishExplains', function () {
   if (win != null) {
-    win.setContentSize(768, 450); 
+    win.setContentSize(768, 450);
     win.center();
   }
 })
@@ -64,7 +74,7 @@ ipcMain.on('window-englishExplains', function () {
 //窗口韦氏词典尺寸，通过ipc传递
 ipcMain.on('window-set', function () {
   if (win != null) {
-    win.setContentSize(384, 512); 
+    win.setContentSize(384, 512);
     win.center();
   }
 })
