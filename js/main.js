18aab288ad8947f36cb6ffc71f675718af329a49
RoderickQiu <scrisqiu@hotmail.com>
2019-01-12 22:41:12 +0800

    1.3.0 update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray, Menu, globalShortcut } = require('electron')
+const Store = require('electron-store');
+const store = new Store();//设置数据读取
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+let tray = null
 
 function createWindow() {
   // 创建浏览器窗口。
@@ -13,7 +16,7 @@ function createWindow() {
 
   //在加载页面时，渲染进程第一次完成绘制时，会发出 ready-to-show 事件。在此事件后显示窗口将没有视觉闪烁
   win.once('ready-to-show', () => {
-    win.show();
+    win.show()
     //win.webContents.openDevTools()
   })
 
@@ -23,24 +26,45 @@ function createWindow() {
     // 通常会把多个 window 对象存放在一个数组里面，
     // 与此同时，你应该删除相应的元素。
     win = null
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
   })
 }
 
-// 当全部窗口关闭时退出。
-app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+// 当程序就要结束
+app.on('will-quit', () => {
+  // 清空所有快捷键
+  globalShortcut.unregisterAll()
 })
 
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', () => {
-  createWindow();
-})
+
+
+  // Electron 会在初始化后并准备
+  // 创建浏览器窗口时，调用这个函数。
+  // 部分 API 在 ready 事件触发后才能使用。
+  app.on('ready', () => {
+    createWindow()
+
+    tray = new Tray('./res/icons/iconWin.ico')
+    const contextMenu = Menu.buildFromTemplate([
+      { label: '退出', click: () => { app.quit() } }
+    ])
+    tray.setToolTip('yDic 你的词典')
+    tray.setContextMenu(contextMenu)
+    tray.on('click', () => {
+      win.isVisible() ? win.hide() : win.show()
+    })//托盘菜单
+
+    var hideOrShowAcc = store.get('hideOrShowAcc');
+    if (hideOrShowAcc == 'undefined' || !hideOrShowAcc) {
+      store.set('hideOrShowAcc', 'Y');
+      hideOrShowAcc = 'Y';
+    }
+    globalShortcut.register('CommandOrControl+Alt+Shift+' + hideOrShowAcc, () => {
+      win.isVisible() ? win.hide() : win.show()
+    })//注册全局快捷键：显示/隐藏
+  })
 
 app.on('activate', () => {
   // 在macOS上，当单击dock图标并且没有其他窗口打开时，
@@ -52,29 +76,21 @@ app.on('activate', () => {
 
 //登录窗口最小化，通过ipc传递
 ipcMain.on('window-min', function () {
-  if (win != null) win.minimize();
+  if (win != null) win.minimize()
 })
 
 //窗口有道词典尺寸，通过ipc传递
 ipcMain.on('window-dict', function () {
   if (win != null) {
-    win.setContentSize(768, 300);
-    win.center();
+    win.setContentSize(768, 300)
+    win.center()
   }
 })
 
 //窗口韦氏词典尺寸，通过ipc传递
 ipcMain.on('window-englishExplains', function () {
   if (win != null) {
-    win.setContentSize(768, 450);
-    win.center();
-  }
-})
-
-//窗口设置尺寸，通过ipc传递
-ipcMain.on('window-set', function () {
-  if (win != null) {
-    win.setContentSize(384, 512);
-    win.center();
+    win.setContentSize(768, 450)
+    win.center()
   }
 })
